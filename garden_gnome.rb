5a81a1d4b951fa0977adeffe476f5339e407c744
galeheidi <kgaletax@aol.com>
2018-06-21 20:46:26 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,12 +1,16 @@
 # Code your instances here
 class GardenGnomes
-  attr_reader :name, :age, :gluten_allergies, :personality
+  attr_accessor :name, :age, :gluten_allergies :hat_color
+  attr_reader :personality 
   def initialize(name, age, gluten_allergy, personality)
     @name = name
     @age = age 
     @gluten_allergy = gluten_allergy
     @personality = evil
-  hat_color = red 
+    @hat_color = hat_color
+    hat_color = red 
   end 
+  def gnaw
+    {
 end 
     
